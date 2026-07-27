463534637e1acd1847d6be4b7ac7f001ea5a0da2
Filipe Cabral <felipe.cabral1933@gmail.com>
2026-07-26 21:39:19 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/filipecabral/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.filipecabral.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/com/filipecabral/StartupTests.java
@@ -0,0 +1,13 @@
+package com.filipecabral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
